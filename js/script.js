d56313ae140d686a9b7478747d43bbdc6964fa9a
aCute5 <costantini95dario@gmail.com>
2022-10-20 15:16:01 +0200

    script-done

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const km = prompt("Quanti KM devi fare?");
+const age = prompt("Quanti anni hai?");
+const kmPrice = ("Il prezzo finale è €" , km * 0.21);
+const kmPriceunder18 = ("Congratulazioni! Hai ottenuto lo sconto del 20%: €", kmPrice * 0.80);
+const kmPriceover60 =  ("Congratulazioni! Hai ottenuto lo sconto del 20%: €", kmPrice * 0.60);
+if (age <= "18"){
+    console.log("Congratulazioni! Hai ottenuto lo sconto del 20% dedicato ai giovani:");
+    console.log("€",kmPriceunder18 .toFixed (2));
+}
+else if(age >= "60"){
+    console.log("Congratulazioni! Hai ottenuto lo sconto del 60% dedicato agli anziani!");
+    console.log ("€",kmPriceover60 .toFixed (2));
+}
+else{
+    console.log("Il prezzo finale è:");
+    console.log("€",kmPrice .toFixed (2));
+    
+}
+
+
+
+
+
+
+
+
+
+
+
